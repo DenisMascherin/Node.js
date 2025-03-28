5226b4d723f4b9e0b671ed28fe423dd55cd7fb57
Denis Mascherin <denis.mascherin.24@stud.itsaltoadriatico.it>
2025-03-28 10:38:55 +0100

    Aggiunto .git.ignore

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World\n');
+
 });
 
 server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}/`);
 });
+
